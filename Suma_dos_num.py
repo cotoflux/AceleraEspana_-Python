f0210550286a744d4d9782a647793b4560a36d4c
cotoflux <cotoflux@hotmail.com>
2019-03-19 13:24:05 +0100

    Primer commit ejercico sumas

---
+++ Suma_dos_num.py
@@ -0,0 +1,7 @@
+num1 = int(input("primer numero: "))
+num2 = int(input("segundo numero: "))
+num3 = int(input("tercer numero: "))
+
+suma = str(num1 + num2 + num3)
+
+print("El resultado es: " + suma )
