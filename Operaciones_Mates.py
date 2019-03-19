aed48397dafb74fdf149804f002262cb225d1c67
cotoflux <35181092+cotoflux@users.noreply.github.com>
2019-03-19 21:26:41 +0100

    Añado el redondeo y los comentarios al programa.
    Adding the round to my formula and the comments to the program.

--- Operaciones_Mates.py
+++ Operaciones_Mates.py
@@ -1,27 +1,31 @@
-
+# Bienvenida pedimos el nombre para personalizar la experiencia. 
 nombre = input('¿Como te llamas? ')
 print('Hola ', nombre)
-print ('Ahora te pediremos dos números y el sistema te devolverà el resultado de su suma, resta, multiplicación y división !')
+print ('Ahora te pediremos dos números enteros y el sistema te devolverà el resultado de su suma, resta, multiplicación y división !')
 
+# Petición al usuario para que entre  los números para hacer el caculo
 petNum1 = input('Introduce un número entero : ')
 petNum2 = input('Introduce otro número entero : ')
+petNum3 = input('Entra el divisor con el que quieres hacer la división: ')
 
+# Convertimos el número entrado a un integer i declaramos el divisor.
 num1 = int(petNum1)
 num2 = int(petNum2)
-
-petNum3 = input('Entra el divisor con el que quieres hacer la división: ')
 divisor = int(petNum3)
 
-sumNum = num1 + num2
-restNum = num1 - num2
-divNum = sumNum / divisor 
-multipNum = num1 * num2
+# Hacemos los cálulos y redondeamos a dos decimales con la formula(round(la formula, 2)
+sumNum = round(num1 + num2, 2)
+restNum = round(num1 - num2, 2)
+divNum = round(sumNum / divisor, 2) 
+multipNum = round(num1 * num2, 2)
 
+# Convertimos los números a string para publicarlos y dar el resultado al usuario.
 strSumNum = str(sumNum)
 strRestNum = str(restNum)
 strDivnum = str(divNum)
 strMultipNum = str(multipNum)
 
+# Imprimimos en pantalla el reultado para el usuario. 
 print('El resultado de la suma de los dos números que has introducido es: ', strSumNum )
 print('El resultado de la resta de los dos números que has introducido es: ', strRestNum )
 print('El resultado de la división de los dos números que has introducido por 10 es: ', strDivnum )
